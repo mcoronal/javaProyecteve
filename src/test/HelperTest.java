717aa3d4a1b2642f221e00a03f42c77d01351574
Izant <56949815+Izant@users.noreply.github.com>
2020-04-24 13:20:37 +0200

    version 1.04 final Test

--- src/test/HelperTest.java
+++ src/test/HelperTest.java
@@ -7,7 +7,32 @@ import org.junit.jupiter.api.Test;
 class HelperTest {
 
 	@Test
-	void test() {
+	void testStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMenuUnico() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAgregarVideojuego() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testListarVideojuegos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEliminarVideojuego() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidacionPlataformas() {
 		fail("Not yet implemented");
 	}
 
